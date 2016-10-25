fb8db1685414a4e85216d77aeabb9ec76847f394
Shuai Yuan <yuans06@unl103593-m.local>
2016-10-26 10:41:49 +1300

    Initial Commit

---
+++ JigsawPuzzle-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JigsawPuzzle-OC
+//
+//  Created by Shuai Yuan on 26/10/16.
+//  Copyright © 2016 Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JigsawPuzzle-OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JigsawPuzzle-OC
+//
+//  Created by Shuai Yuan on 26/10/16.
+//  Copyright © 2016 Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
